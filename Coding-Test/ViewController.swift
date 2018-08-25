bf5ac6ba5949713325f880c984f734dd9235ff27
noushad-clickapps <sahil.clb@gmail.com>
2018-08-25 11:45:32 +0530

    Initial Commit

---
+++ Coding-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coding-Test
+//
+//  Created by Aman Aggarwal on 25/08/18.
+//  Copyright © 2018 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
